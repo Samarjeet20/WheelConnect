854440d2596814406e84663a5c21f026c7f1c7bb
AnujY77111 <anujy2689@gmail.com>
2025-08-16 11:14:19 +0530

    updated middlewares

--- Backend/middlewares/admin.middleware.js
+++ Backend/middlewares/admin.middleware.js
@@ -4,17 +4,18 @@ const Admin = require('../models/admin.model');
 // Authentication middleware
 const adminMiddleware = async (req, res, next) => {
     let token = req.headers.authorization;
-    if(!token) return res.status(401).json({message: "Unauthorized"})
+    if(!token) return res.status(401).json({message: "Unauthorized Access"})
     token = token.split(" ")[1];
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await Admin.findOne({email: decoded.email}).lean();
-        if(user.usertype != 'Admin') return res.status(401).json({message: "Unauthorized"});
+        if(user.usertype != 'Admin') return res.status(401).json({message: "Unauthorized Access"});
         req.user = {...user, id: user._id};
         next()
 
     }catch(err){
-        return res.status(401).json({message: "Unauthorized"})
+
+        return res.status(401).json({message: "Unauthorized access"})
     }
 
 }
