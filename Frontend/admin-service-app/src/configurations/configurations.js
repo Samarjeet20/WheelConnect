dd4b4753c7ee06696c2b08c441b96cd9fa3c5f68
Samarjeet20 <samarjeetshinde20@gmail.com>
2025-08-19 21:27:04 +0530

    testing?

---
+++ Frontend/admin-service-app/src/configurations/configurations.js
@@ -0,0 +1,2 @@
+export const GOOGLE_MAP_API = REACT_APP_GOOGLE_API_KEY;
+export const baseUrl = "/api/maps";
---
+++ Frontend/client-app/Credentials/credentials.js
@@ -0,0 +1,2 @@
+export const GOOGLE_API_KEY  = REACT_APP_GOOGLE_API_KEY;
+export const baseUrl = "/api/map";
